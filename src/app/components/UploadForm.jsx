ac268681016b8263061133b24a272a922f210baf
rons9n <rons9n@gmail.com>
2024-01-02 17:58:27 -0500

    PageHeaders component

--- src/app/components/UploadForm.jsx
+++ src/app/components/UploadForm.jsx
@@ -33,7 +33,7 @@ export default function UploadForm() {
           </div>
         </div>
       )}
-      <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 cursor-pointer">
+      <label className="bg-purple-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 cursor-pointer">
         <UploadIcon />
         <span>Choose file</span>
         <input onChange={upload} type="file" className="hidden" />
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import UploadForm from "@/components/UploadForm";
+import PageHeaders from "@/components/PageHeaders";
 
 export default function Home() {
   return (
     <>
+      <PageHeaders
+        h1Text={"Add epic captions to your videos"}
+        h2Text={"Just upload your video and we will do the rest"}
+      />
       <div className="text-center">
         <UploadForm />
       </div>
